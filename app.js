4f4a32ee6acdb228db4a5d7b9d6f46aeea075047
smccourtb <shawnmccourtb@gmail.com>
2021-09-02 14:02:05 -0400

    add complete basic functionality. Can add, multiply, divide, and subtract as you go as long as you need to. Reowrked divide by zero error.

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ function getName() {
 
 let displayValue = 0
 let operator
-let firstNumber = 0, secondNumber = 0
+let firstNumber = 0, secondNumber = null
 let interimNum1, interimNum2, interimNum3
 
 const numbers = document.getElementsByClassName("number")
@@ -34,49 +34,96 @@ const clear = document.querySelector("#clear")
 const equals = document.querySelector("#result")
 const operators = document.getElementsByClassName('operator')
 
+
+// Go through number buttons and and click event to check whether to add to first or second number and display
 for(let i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener('click', () => {
-        display.textContent += numbers[i].name;
-        displayValue += numbers[i].name
-        if(!operator) {
-            firstNumber += numbers[i].name
+        if(!firstNumber && !secondNumber) {
+            display.textContent = ""
         }
-        else{
-            secondNumber += numbers[i].name
+        if(!firstNumber || !operator){
+            
+            firstNumber += numbers[i].name;
+            display.textContent += numbers[i].name;
+            console.log("Storing first number: ", firstNumber)
         }
-        
+        else {
+            if(!secondNumber) {
+                display.textContent = ""
+                secondNumber = numbers[i].name;
+            }
+            else {
+                secondNumber += numbers[i].name;
+            };
+            display.textContent += numbers[i].name;
+            console.log("Storing second number: ", secondNumber)
+        };
     });
-    
 };
 
+// Go through operators and add click event to check if operation needs to be performed
 for(let i = 0; i < operators.length; i++) {
     operators[i].addEventListener('click', () => {
-        display.textContent += operators[i].id;
-        displayValue += operators[i].id
-        operator = operators[i].name
-        // if operator pressed automatically perform function and 
-        //store it in a variable keep doing this with separate variables 
-        //until equals is hit
+
+        displayValue = operators[i].id
+        checkForOperation(operators[i].name)
+
+        // checks for division by zero and returns error
+        
     })
 };
 
-clear.addEventListener('click', cleared);
-equals.addEventListener("click", () => {
-    if(firstNumber && secondNumber && operator) {
-        interimNum1 = operate(firstNumber, secondNumber, operator)
-        firstNumber = interimNum1;
-        secondNumber = 0;
-        operator = null;
-        display.textContent = interimNum1
-    };
-})
 
+// equals.addEventListener("click", () => {
+//     if(operator === 'divide' && secondNumber==0) {
+//         display.textContent = "5318008"
+//         return alert('5318008')
+//         // make it return 5318008
+//     }
+//     if(firstNumber && secondNumber && operator) {
+//         checkForOperation()
+//     };
+// })
+
+clear.addEventListener('click', cleared);
 
 function cleared() {
     display.textContent = null;
     displayValue = "";
     firstNumber = 0;
-    secondNumber = 0;
+    secondNumber = null;
     interimNum1 = 0;
     operator = null;
+};
+
+function checkForOperation(operation) {
+    if(operator === "equals") {
+        
+        interimNum1 = operate(firstNumber, secondNumber, operator)
+        display.textContent = interimNum1
+        operator = null
+        secondNumber = null
+        return
+    };
+    if(operator && secondNumber== "0") {
+        console.log('its true its true')
+        display.textContent = "5318008"
+        return alert('5318008')
+        // it would be cool to flip the calculator upside down like kids back in the day
+    }
+
+    if(firstNumber && secondNumber && operator) {
+        console.log('yerrp')
+        interimNum1 = operate(firstNumber, secondNumber, operator)
+        // operator = null;
+        firstNumber = interimNum1;
+        secondNumber = null;
+        display.textContent = interimNum1
+        // return
+    };
+
+    if(!operator || operation !==operator) {
+        console.log("Storing Operator = ", operation)
+        operator = operation
+    };   
 };
