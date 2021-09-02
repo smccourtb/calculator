cfda0a8de9dcf7a7e218bcad6a4fda2ba140c35a
smccourtb <shawnmccourtb@gmail.com>
2021-09-02 11:21:06 -0400

    add working buttons that display and add working operator buttons. Can now do 1 set of operations

--- app.js
+++ app.js
@@ -1,23 +1,75 @@
-const add = function(a, b) {
+function add(a, b) {
     return a + b;
 };
-
-const subtract = function(a, b) {
+function subtract(a, b) {
     return a - b;
 };
 
-const multiply = function(a, b) {
+function multiply(a, b) {
     return a * b;
 };
 
-const divide = function(a, b) {
+function divide(a, b) {
     return a / b;
 };
 
 
-const operate = function(a, b, operator){
-    return operator(a, b)
-}
+function operate(a, b, operator){
+    return this[operator](parseInt(a), parseInt(b));
+};
 
 
-console.log(operate(10,5, divide))
+function getName() {
+    console.log(this.id)
+};
+
+let displayValue = 0
+let operator
+let firstNumber = 0, secondNumber = 0
+let interimNum1, interimNum2, interimNum3
+
+const numbers = document.getElementsByClassName("number")
+const display = document.querySelector("h3")
+const clear = document.querySelector("#clear")
+const equals = document.querySelector("#result")
+const operators = document.getElementsByClassName('operator')
+
+for(let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', () => {
+        display.textContent += numbers[i].name;
+        displayValue += numbers[i].name
+        if(!operator) {
+            firstNumber += numbers[i].name
+        }
+        else{
+            secondNumber += numbers[i].name
+        }
+        
+    });
+    
+};
+
+for(let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener('click', () => {
+        display.textContent += operators[i].id;
+        displayValue += operators[i].id
+        operator = operators[i].name
+    })
+};
+
+clear.addEventListener('click', cleared);
+equals.addEventListener("click", () => {
+    if(firstNumber && secondNumber && operator) {
+        interimNum1 = operate(firstNumber, secondNumber, operator)
+    };
+})
+
+
+function cleared() {
+    display.textContent = null;
+    displayValue = ""
+    firstNumber = 0
+    secondNumber = 0
+    interimNum1 = 0
+    // will have to clear variables in this at some point
+};
